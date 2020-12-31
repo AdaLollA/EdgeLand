78baf88d5874c2707eed2b73662a4db9b32004ee
Lorenz Graf <lgraf@lightcyde.agency>
2020-12-31 17:20:52 +0100

    walls

--- Camera.gd
+++ Camera.gd
@@ -1,11 +1,13 @@
 extends Camera2D
 
 export (float) var granularity = 0.1
+export (float) var maxZoom = 1
+export (float) var minZoom = 0.1
 
 func _input(event):
-	if event.is_action_pressed('zoom_out') && zoom.x < 1:
+	if event.is_action_pressed('zoom_out') && zoom.x < maxZoom:
 		zoom.x += granularity
 		zoom.y += granularity
-	elif event.is_action_pressed('zoom_in') && zoom.x > 0.5:
+	elif event.is_action_pressed('zoom_in') && zoom.x > minZoom:
 		zoom.x -= granularity
 		zoom.y -= granularity
