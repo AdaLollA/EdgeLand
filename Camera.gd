fa55b10cd5be1a06677f7c48df04cade3d85bfd3
Lorenz Graf <lgraf@lightcyde.agency>
2021-01-01 14:09:46 +0100

    drag and drop camera movement

--- Camera.gd
+++ Camera.gd
@@ -4,10 +4,32 @@ export (float) var granularity = 0.1
 export (float) var maxZoom = 1
 export (float) var minZoom = 0.1
 
+var previousDragPosition = null
+var drag = false
+
+
 func _input(event):
+	# zoom
 	if event.is_action_pressed('zoom_out') && zoom.x < maxZoom:
 		zoom.x += granularity
 		zoom.y += granularity
 	elif event.is_action_pressed('zoom_in') && zoom.x > minZoom:
 		zoom.x -= granularity
 		zoom.y -= granularity
+	
+	# movement
+	if event.is_action_pressed('camera_drag'):
+		previousDragPosition = get_local_mouse_position()
+		drag = true
+	elif event.is_action_released('camera_drag'):
+		drag = false
+
+func _physics_process(delta):
+	if drag:
+		var deltaTransform = previousDragPosition - get_local_mouse_position()
+		previousDragPosition = get_local_mouse_position()
+		transform.origin += deltaTransform
+		print(deltaTransform)
+
+func _process(delta):
+	pass
