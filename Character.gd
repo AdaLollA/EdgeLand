9da62f190b2ce4220718a23c154d429ed4d6b1e9
Lorenz Graf <lgraf@lightcyde.agency>
2021-01-01 21:52:37 +0100

    ai pathing with drawn line

--- Character.gd
+++ Character.gd
@@ -2,9 +2,8 @@ extends KinematicBody2D
 
 var bullet = preload("res://Bullet.tscn")
 
-# mechanical
+# mechanics
 export (int) var faction = 0
-export (int) var speed = 50
 export (int) var health = 100
 
 # visual
@@ -13,6 +12,8 @@ export (float) var width = 0.7
 export (Color) var color = Color('F1C27D')
 
 # movement
+export (int) var speed = 50
+var path = PoolVector2Array() setget setPath
 var target = Vector2()
 var velocity = Vector2()
 
@@ -21,20 +22,27 @@ var progress = 100
 var selected = false
 
 func _ready():
+	set_process(false)
 	target = get_transform().get_origin()
 	fire()
 
 func _input(event):
 	if event.is_action_pressed('right_click') && selected:
-		var tile = get_global_mouse_position() / 16
-		tile = Vector2(int(tile.x), int(tile.y)) + Vector2(0.5, 0.5)
-		target = tile * 16
+		moveTo(get_global_mouse_position())
 
-func _physics_process(delta):
-	velocity = position.direction_to(target) * speed
-	# look_at(target)
-	if position.distance_to(target) > 1:
-		velocity = move_and_slide(velocity)
+func moveTo(value: Vector2):
+	var tile = value / 16
+	tile = Vector2(int(tile.x), int(tile.y)) + Vector2(0.5, 0.5)
+	target = tile * 16
+	var newPath = get_node("/root/root/Navigation").get_simple_path(global_position, target)
+	get_node("/root/root/GameManager").showPath(newPath)
+	setPath(newPath)
+
+#func _physics_process(delta):
+#	velocity = position.direction_to(target) * speed
+#	# look_at(target)
+#	if position.distance_to(target) > 1:
+#		velocity = move_and_slide(velocity)
 
 func fire():
 	var bulletInstance = bullet.instance()
@@ -57,3 +65,28 @@ func _on_ClickBox_input_event(viewport, event, shape_idx):
 
 func updateUI():
 	$CharacterUI.updateNameUI()
+
+func setPath(value: PoolVector2Array):
+	path = value
+	if value.size() == 0:
+		return
+	set_process(true)
+
+func _process(delta):
+	var move_distance = speed * delta
+	move_along_path(move_distance)
+
+func move_along_path(distance: float):
+	var start_point = position
+	for i in range(path.size()):
+		var distance_to_next = start_point.distance_to(path[0])
+		if distance <= distance_to_next and distance >= 0:
+			position = start_point.linear_interpolate(path[0], distance / distance_to_next)
+			break
+		elif path.size() == 1 and distance > distance_to_next:
+			position = path[0]
+			set_process(false)
+			break
+		distance += distance_to_next
+		start_point = path[0]
+		path.remove(0)
--- GameManager.gd
+++ GameManager.gd
@@ -4,18 +4,28 @@ var character = preload("res://Character.tscn")
 
 var colors = ['FFDBAC', 'F1C27D', 'E0AC69', 'C68642', '8D5524']
 
+# counter for unique path ids
+var nextPathId = 0
+
 func _ready():
 	randomize()
-	spawnCharacter('Gideon', Vector2(50,50), 0)
+	spawnCharacter('Gideon', Vector2(100,100), 0, true)
 	spawnCharacter('Ann', Vector2(200,50), 1)
 	pass 
 
-func spawnCharacter(name, position, faction):
+func spawnCharacter(name, position, faction, selected = false):
 	var newCharacter = character.instance()
 	newCharacter.position = position
 	newCharacter.width = rand_range(0.3, 0.8)
 	newCharacter.color = colors[randi()%5]
 	newCharacter.faction = faction
 	newCharacter.name = name
-	
+	newCharacter.selected = selected
 	$Entities.add_child(newCharacter)
+
+func showPath(path: PoolVector2Array) -> int:
+	var line : Line2D = Line2D.new()
+	line.points = path
+	$Paths.add_child(line, nextPathId)
+	nextPathId += 1
+	return nextPathId-1
