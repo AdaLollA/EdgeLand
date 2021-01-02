5184b27b450cd63df97feeab39676fa363f177fe
Lorenz Graf <lgraf@lightcyde.agency>
2021-01-02 18:59:54 +0100

    weapon hello world

--- gear/Weapon.gd
+++ gear/Weapon.gd
@@ -1,8 +1,8 @@
 extends Node2D
 
-export (int) var accuracy = 123
-export (int) var knock_back = 123
-export (int) var range_distance = 123
-export (int) var charge_time = 123
-export (int) var fire_rate = 123
-export (int) var burst_size = 123
+export (int) var accuracy = 90
+export (int) var knock_back = 1
+export (int) var range_distance = 100
+export (int) var charge_time = 1
+export (int) var fire_rate = 10
+export (int) var burst_size = 3
