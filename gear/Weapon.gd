7c4ec74cbf269bfccabd9e53e694bf27431536a6
Lorenz Graf <lgraf@lightcyde.agency>
2021-01-02 21:02:59 +0100

    weapon handle and idle position

--- gear/Weapon.gd
+++ gear/Weapon.gd
@@ -6,3 +6,14 @@ export (int) var range_distance = 100
 export (int) var charge_time = 1
 export (int) var fire_rate = 10
 export (int) var burst_size = 3
+
+var idle_rotation = 45
+var idle_position = Vector2(6,-5)
+
+func _ready():
+	position -= $Handle.position
+	take_idle_position()
+
+func take_idle_position():
+	rotate(deg2rad(idle_rotation))
+	position -= idle_position
