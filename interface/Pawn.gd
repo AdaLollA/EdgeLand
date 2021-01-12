ded131c9dba66f46b7ba34131962fa969268de15
Lorenz Graf <lgraf@lightcyde.agency>
2021-01-12 19:41:57 +0100

    pawn ui bugfix and setter impl

--- interface/Pawn.gd
+++ interface/Pawn.gd
@@ -1,12 +1,20 @@
 extends MarginContainer
 
 var pawn_name = 'Name' setget set_pawn_name
+var pawn_mood = 70 setget set_pawn_mood
+var pawn_health = 70 setget set_pawn_health
 
 func set_pawn_name(value):
 	pawn_name = value
 	# todo subscribe to selcted signal to update ui with underline when needed
 	$Control/NameLabel.bbcode_text = '[center]' + pawn_name
 
-# todo mood bar setter
+func set_pawn_mood(value):
+	pawn_mood = value
+	# todo subscribe to mood change signal
+	$Control/MoodBar.value = pawn_mood
 
-# todo health bar setter
+func set_pawn_health(value):
+	pawn_health = value
+	# todo subscribe to mood change signal
+	$Control/HealthBar.value = pawn_health
